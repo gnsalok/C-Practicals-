77d67653c28ed62ca3bd936772f76479d930d0f6
Alok Tripathi <gnsalok@outlook.com>
2017-09-29 01:09:05 +0530

    working with delegate and events...

---
+++ Events/Events/MailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EventsAndDelegate
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("Mail Service : Sending an Email...");
+        }
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EventsAndDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { title = "Video 1" };
+            var videoEncoder = new VideoEncoder();  //publisher
+            var mailService = new MailService();    //subscriber
+            var textMessageService = new TextMessageService();  //subscriber
+
+
+            //OnVideoEncoded is reference to the mail service method...
+            //just like multicast
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += textMessageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
