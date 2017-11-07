bb7d9e2c3110f96974865833e42799494151aaad
Alok Tripathi <gnsalok@outlook.com>
2017-11-07 21:12:08 +0530

    working wih extension methods

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -12,6 +12,11 @@ namespace ExtensionMethods
         {
             string post = "This is supposed to be a very long blog post blah blah blah...";
             var shortenedPost = post.Shorten(5);
+            Console.WriteLine(shortenedPost);
+
+            string s = "Hello Extension Methods";
+            int i = s.Length;
+            Console.WriteLine( "Length of string is "+i);
 
             IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
             var max = numbers.Max();
