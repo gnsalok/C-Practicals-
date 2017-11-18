b89ebaad300956e0e914bc75d591852cba1b2736
Alok Tripathi <gnsalok@outlook.com>
2017-11-18 19:43:30 +0530

    working with Func delegate

---
+++ FuncDelegate/FuncDelegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<string, string> convert = s => s.ToUpper();
+
+            //string name = "Sample Text";
+
+            //Console.WriteLine(convert(name));
+
+
+            Func<string, string> convert = delegate (string s)
+            { return s.ToUpper(); };
+
+            string name = "Dakota";
+            Console.WriteLine(convert(name));
+
+
+
+            Func<int, int> cube = delegate (int c)
+               {
+                   return c * c * c;
+               };
+
+            Console.WriteLine("Enter a value to find cube?");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The cube is :"+cube(3));
+            
+            
+        }
+    }
+}
